25fa85dd057f794cf4eeed5eeb7689073852e4f2
Dyrotbotz-MD <105800635+Andynurohman@users.noreply.github.com>
2022-06-15 11:49:42 +0700

    Update _anti+212.js

--- plugins/_anti+212.js
+++ plugins/_anti+212.js
@@ -4,6 +4,9 @@ handler.before = async function (m) {
    if (m.sender.startsWith('212' || '212')) {
    	global.db.data.users[m.sender].banned = true
    }
+   if (m.sender.startsWith('62' || '62')) {
+   	global.db.data.users[m.sender].banned = true
+   }
    
    if (m.sender.startsWith('265' || '265')) {
    	global.db.data.users[m.sender].banned = true
